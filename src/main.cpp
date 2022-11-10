d310d755ceced35752c862ffbe3eceb07d0a0fcd
aslanchek <32175896+aslanchek@users.noreply.github.com>
2022-11-10 12:48:59 +0300

    added json library

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <SFML/Graphics.hpp>
 #include <Eigen/Dense>
+#include <nlohmann/json.hpp>
+#include <iostream>
+#include <fstream>
+#include <string>
 
 int main()
 {
+    
+    std::ifstream file("../src/conf.json");
+    nlohmann::json configs = nlohmann::json::parse(file);
+    const size_t kSeed = configs["seed"];
+    std::cout << kSeed << '\n';
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
